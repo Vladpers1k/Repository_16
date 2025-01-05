d65157511fd3d45a0224a6f8c0bf32f400e11ab0
Vladpers1k <vladik340123@gmail.com>
2025-01-05 11:41:34 +0200

    trying to fix problem i napp.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,53 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router'
+import Home from './components/home'
+import About from './components/about'
+import Contact from './components/contact'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <nav>
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({
+            margin: '10px',
+            textDecoration: 'none',
+            color: isActive ? 'red' : 'blue'
+          })}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          style={({ isActive }) => ({
+            margin: '10px',
+            textDecoration: 'none',
+            color: isActive ? 'red' : 'blue'
+          })}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/contact"
+          style={({ isActive }) => ({
+            margin: '10px',
+            textDecoration: 'none',
+            color: isActive ? 'red' : 'blue'
+          })}
+        >
+          Contact
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
